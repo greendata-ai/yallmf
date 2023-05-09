15413adeb9adea75791405244356094e76e4b0a2
Kyle Napierkowski <knapierkowski@gmail.com>
2023-05-09 14:13:16 -0700

    prompt to choose from list

--- yallmf/llm.py
+++ yallmf/llm.py
@@ -2,6 +2,8 @@
 Scaffolding to make it easier to work with LLMs
 '''
 
+import re
+import string
 from enum import Enum
 from dataclasses import dataclass, asdict, field
 import pandas as pd
@@ -15,6 +17,82 @@ class Prompt:
     max_length: int = 4096*WORDS_PER_TOKEN # for chat-gpt-3.5;
     # gpt-4 8192*4, gpt-4-32k 32768*4
     temperature: float = 0.5
+
+    def validate_clean(self, completion):
+        '''
+        Return only the message
+        '''
+        pass
+
+
+@dataclass(kw_only=True)
+class ChoicesListPrompt(Prompt):
+    '''
+    Choose one or more values from a list
+    '''
+    choices: list = field(default_factory=list)
+    doc_tag: str ='\n\n%%DOCUMENT:\n'
+    choices_tag: str ='\n\n%%CHOICES:\n'
+    result_tag: str = '\n\n%%RESULT:\n'
+    strip_punct: bool = True
+    lowercase: bool = True
+    correction_map: dict = field(default_factory=dict)
+    skipvalues: set = field(default_factory=set)
+    temperature: float = 0.1
+
+    def validate_clean(self, completion):
+        '''Return list of valid values, asking the LLM to correct an invalid value if possible'''
+
+        v = completion['choices'][0]['message']['content']
+        vals = [val.strip() for val in v.split(',')]
+        if self.strip_punct:
+            vals = [val.strip(string.punctuation) for val in vals]
+        if self.lowercase:
+            vals = [val.lower() for val in vals]
+        final = set()
+        # correct values not in `choices`
+        for v in vals:
+            if v in self.choices:
+                final.add(v)
+            else:
+                if v in self.skipvalues:
+                    continue
+                if v in self.correction_map.keys():
+                    final.add(self.correction_map[v])
+                else: # ask for a new value n times
+                    maxtries=5
+                    i=0
+                    while i<maxtries:
+                        print(f'fixing choice "{v}"...')
+                        prompt = f'''You are a machine that chooses a single item from a list. What item from CHOICES most closely matches "{v}"? ONLY RETURN THE MATCHING "CHOICES" VALUE BY ITSELF.''' \
+                            +self.choices_tag+', '.join(self.choices)+'\n\n%%CHOICE:'
+                        comp = chat_completion(prompt,timeout=10)
+                        newv = comp['choices'][0]['message']['content'].strip()
+                        newv = newv.strip(string.punctuation)
+                        if lowercase:
+                            newv = newv.lower()
+                        if newv in self.choices:
+                            print(f'Successfully matched {v} to {newv}')
+                            final.add(newv)
+                            self.correction_map[v]=newv
+                            break
+                        else:
+                            print(f'BAD: {newv}')
+                            i+=1
+                    if i==maxtries:
+                        print(f'Related lookup not successful after {maxtries} tries. Adding "{v}" to `skipvalues` set.')
+                        self.skipvalues.add(v)
+        return sorted(list(final))
+    def get_prompt(self, doc):
+        return self.base_prompt+self.doc_tag+doc+self.choices_tag+', '.join(self.choices)+self.result_tag
+    def process_doc(self, doc):
+        prompt = self.get_prompt(doc)
+        completion = chat_completion(prompt)
+        return self.validate_clean(completion)
+    def process_docs_gen(self, doclist):
+        pass
+
+
 
 @dataclass(kw_only=True)
 class JsonPrompt(Prompt):
